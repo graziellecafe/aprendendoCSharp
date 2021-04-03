3ce31082d7d56d6072799dc81337b86e849e7c66
graziellecafe <graziellecafe@users.noreply.github.com>
2021-04-03 14:23:47 -0300

    Create Program.cs

---
+++ 3-criandoVariaveisFlutuante/3-criandoVariaveisFlutuante/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_criandoVariaveisFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Criando projeto 3 -- Criando variáveis ponto flutuante");
+            
+            double salario = 1118.50;
+            Console.WriteLine(salario);
+            salario = salario / 2;
+            Console.WriteLine(salario);
+
+            Console.WriteLine("Tecle enter para sair");
+            Console.Read();
+
+
+        }
+    }
+}
