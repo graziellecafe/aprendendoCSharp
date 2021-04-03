080ca5b72b80d3459f2ccb6e377aa3d1feb1b759
graziellecafe <graziellecafe@users.noreply.github.com>
2021-04-03 16:51:20 -0300

    Create Program.cs

---
+++ 5-exercicio/5-exercicio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parcela_1 = "10";
+            string parcela_2 = "20";
+
+            Console.WriteLine(parcela_1 + parcela_2);
+            Console.Read();
+        }
+    }
+}
