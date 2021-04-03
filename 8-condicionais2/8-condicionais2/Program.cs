d23058a1e9b43738ecb6723a783aa73d7857a16c
graziellecafe <graziellecafe@users.noreply.github.com>
2021-04-03 16:51:45 -0300

    Create Program.cs

---
+++ 8-condicionais2/8-condicionais2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 27;
+            int quantidadeDePessoas = 1;
+            if (idade >= 18 && quantidadeDePessoas >=2)
+            {
+                Console.WriteLine("Você PODE entrar!");
+            }
+            else
+            {
+                Console.Write("Você NÃO pode entrar");
+            }
+            Console.Read();
+        }
+    }
+}
