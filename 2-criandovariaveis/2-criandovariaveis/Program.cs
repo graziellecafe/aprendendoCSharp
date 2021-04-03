199a8dab5d0b87ce31cf8e7d276e6f1a0be9a5c9
graziellecafe <graziellecafe@users.noreply.github.com>
2021-04-03 14:23:37 -0300

    Create Program.cs

---
+++ 2-criandovariaveis/2-criandovariaveis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_criandovariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declaração de variáveis
+            int idade = 27;
+            Console.WriteLine("Você tem " + idade + " anos");
+            Console.WriteLine("Olá mundo!");
+            Console.ReadLine();
+        }
+    }
+}
