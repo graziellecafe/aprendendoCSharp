de648049af025080ed1cdcb868142c43a73ac616
graziellecafe <graziellecafe@users.noreply.github.com>
2021-04-03 19:57:16 -0300

    Create Program.cs

---
+++ 9-calculaPoupanca/9-calculaPoupanca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_calculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcula Poupança");
+            double valorInvestido = 1000.00;
+
+            int i = 1; 
+            while (i <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * (0.36 / 100);
+                Console.WriteLine("Após " + i + " mes(es), você terá R$ " + valorInvestido);
+                i++;
+            }
+
+
+            Console.Read();
+        }
+    }
+}
