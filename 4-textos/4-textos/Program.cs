20f8cd995a189a7ef95e9733f5a0d8facce7fe3b
graziellecafe <graziellecafe@users.noreply.github.com>
2021-04-03 14:38:12 -0300

    Create Program.cs

---
+++ 4-textos/4-textos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_textos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titulo = "Alura Cursos de Tecnologia";
+            string paragrafo = "Curso de CSharp Introdutório";
+            Console.WriteLine(titulo);
+            Console.WriteLine(paragrafo);
+
+            Console.WriteLine("Para finalizar, aperte enter");
+            Console.Read();
+        }
+    }
+}
