4a14c4861b98a7bf25b4657dc85212e1f87895b8
graziellecafe <graziellecafe@users.noreply.github.com>
2021-04-03 16:51:37 -0300

    Create Program.cs

---
+++ 7-condicionais/7-condicionais/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 27; 
+
+            if(idade >= 18)
+            {
+                Console.Write("Você tem mais de 18 anos de idade!");
+            }
+            else
+            {
+                Console.Write("Você tem menos de 18 anos");
+            }
+
+            Console.Read();
+        }
+    }
+}
